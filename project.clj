8c041b5587b37897aed6e6957c98c351024517bb
Christopher Martin <cgmartin@gmail.com>
2013-06-09 02:46:39 -0400

    Travis configuration updates

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "https://github.com/cgmartin/clj-wamp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.2"]
                  [org.clojure/tools.logging "0.2.6"]
                  [http-kit "2.1.3"]
                  [cheshire "5.2.0"]]
-  :min-lein-version "2.0.0")
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
